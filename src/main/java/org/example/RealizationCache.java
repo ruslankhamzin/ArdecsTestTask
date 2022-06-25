ff0a736e00dcf48500b7477f7cb26d9c0bb9304e
Ruslan Khamzin <hamzin362@gmail.com>
2022-06-25 22:50:01 +0400

    Debug

--- src/main/java/org/example/RealizationCache.java
+++ src/main/java/org/example/RealizationCache.java
@@ -4,23 +4,24 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 
 public class RealizationCache implements Cache {
     String fileName;
     String filename2;
     int cacheSize;
-    HashMap map;
-    LinkedList list;
-    HashMap map1;
+    HashMap<String, String> map;
+    LinkedList<String> list;
+    HashMap<String, Integer> map1;
     int strategy;
     int repo;
 
     public RealizationCache(int cacheSize, int strategy, int repo) {
         this.cacheSize = cacheSize;
-        map = new HashMap(cacheSize);
+        map = new HashMap<>(cacheSize);
         this.strategy = strategy;
-        map1 = new HashMap(cacheSize);
-        list = new LinkedList();
+        map1 = new HashMap<>(cacheSize);
+        list = new LinkedList<>();
         this.repo = repo;
         switch (strategy) {
             case 1:
@@ -28,7 +29,6 @@ public class RealizationCache implements Cache {
                 filename2 = "LRUHelpCacheRepo.txt";
                 break;
             case 2:
-
                 fileName = "MRUCacheRepo.txt";
                 filename2 = "MRUHelpCacheRepo.txt";
                 break;
@@ -43,7 +43,7 @@ public class RealizationCache implements Cache {
     public void put(String key, String value) {
         switch (strategy) {
             case 1:
-                boolean res = list.remove(key);
+                list.remove(key);
                 while (list.size() >= this.cacheSize) {
                     this.pruning();
                 }
@@ -94,13 +94,11 @@ public class RealizationCache implements Cache {
                 }
                 break;
             case 3:
-                boolean res3 = false;
-                res3 = map.containsKey(key);
-               boolean res4 = map1.containsKey(key);
-                if (res3 && res4) {
-                    int value = (int) map1.get(key);
+                boolean res3 = map1.containsKey(key);
+                if (res3) {
+                    int value = map1.get(key);
                     value++;
-                    map1.put(key,value);
+                    map1.put(key, value);
                     if (repo == 1) {
                         cacheOnDisk();
                     }
@@ -120,9 +118,9 @@ public class RealizationCache implements Cache {
 
     @Override
     public void clear() {
-        map = new HashMap(cacheSize);
-        list = new LinkedList();
-        map1= new HashMap(cacheSize);
+        map = new HashMap<>(cacheSize);
+        list = new LinkedList<>();
+        map1 = new HashMap<>(cacheSize);
         if (repo == 1) {
             try {
                 FileWriter fwOb = new FileWriter(fileName, false);
@@ -145,66 +143,45 @@ public class RealizationCache implements Cache {
 
     @Override
     public void cacheOnDisk() {
-        switch (strategy) {
-
-            case 3:
-                try {
-                    FileOutputStream fos = new FileOutputStream(fileName);
-                    ObjectOutputStream oos = new ObjectOutputStream(fos);
-                    oos.writeObject(map);
-                    oos.close();
-                    fos = new FileOutputStream(filename2);
-                    oos = new ObjectOutputStream(fos);
-                    oos.writeObject(map1);
-                    oos.close();
-                } catch (IOException e) {
-                    System.out.println("Произошла ошибка ввода-вывода");
-                }
-                break;
-
-
-            default:
-                try {
-                    FileOutputStream fos = new FileOutputStream(fileName);
-                    ObjectOutputStream oos = new ObjectOutputStream(fos);
-                    oos.writeObject(map);
-                    oos.close();
-                    fos = new FileOutputStream(filename2);
-                    oos = new ObjectOutputStream(fos);
-                    oos.writeObject(list);
-                    oos.close();
-                } catch (IOException e) {
-                    System.out.println("Произошла ошибка ввода-вывода");
-                }
-                break;
+        try {
+            FileOutputStream fos = new FileOutputStream(fileName);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(map);
+            oos.close();
+            fos = new FileOutputStream(filename2);
+            oos = new ObjectOutputStream(fos);
+            if (strategy == 3) {
+                oos.writeObject(map1);
+            } else {
+                oos.writeObject(list);
+            }
+            oos.close();
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка ввода-вывода");
         }
 
-    }
 
+    }
 
     @Override
     public void fromDiskToCache() {
         try {
             FileInputStream fis = new FileInputStream(fileName);
             ObjectInputStream ois = new ObjectInputStream(fis);
-            HashMap mapFromDisk = (HashMap) ois.readObject();
+            HashMap<String, String> mapFromDisk = (HashMap<String, String>) ois.readObject();
             ois.close();
             map = mapFromDisk;
-            FileInputStream fis1 = new FileInputStream(filename2);
-            ObjectInputStream ois2 = new ObjectInputStream(fis1);
-            switch (strategy) {
-                case 1:
-                    LinkedList listFromDisk = (LinkedList) ois2.readObject();
-                    list = listFromDisk;
-                    break;
-                case 3:
-                    HashMap mapCount = (HashMap) ois2.readObject();
-                    if(mapCount!=null) {
-                        map1 = mapCount;
-                    }
-                    break;
+            fis = new FileInputStream(filename2);
+            ois = new ObjectInputStream(fis);
+            if (strategy == 3) {
+                HashMap<String, Integer> mapCount = (HashMap<String, Integer>) ois.readObject();
+                if (mapCount != null) {
+                    map1 = mapCount;
+                }
+            } else {
+                list = (LinkedList<String>) ois.readObject();
             }
-            ois2.close();
+            ois.close();
 
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
@@ -216,29 +193,25 @@ public class RealizationCache implements Cache {
     }
 
     public void pruning() {
-        switch (strategy) {
-            case 3:
-                ArrayList values = new ArrayList(map1.values());
-                int min = (int) values.get(0);
-                for (int i = 1; i < values.size(); i++) {
-                    if (min >= (int) values.get(i)) {
-                        min = (int) values.get(i);
-                    }
+        if (strategy == 3) {
+            List<Integer> values = new ArrayList<>(map1.values());
+            int min = values.get(0);
+            for (int i = 1; i < values.size(); i++) {
+                if (min >= values.get(i)) {
+                    min = values.get(i);
                 }
-                ArrayList listOfKeys = new ArrayList(map1.keySet());
-                for (int i = 0; i < listOfKeys.size(); i++) {
-                    if ((int) map1.get(listOfKeys.get(i)) == min) {
-                        map.remove(listOfKeys.get(i));
-                        map1.remove(listOfKeys.get(i));
-                        break;
-                    }
+            }
+            List<String> listOfKeys = new ArrayList<>(map1.keySet());
+            for (String listOfKey : listOfKeys) {
+                if (map1.get(listOfKey) == min) {
+                    map.remove(listOfKey);
+                    map1.remove(listOfKey);
+                    break;
                 }
-
-                break;
-            default:
-                Object key = list.removeLast();
-                map.remove(key);
-                break;
+            }
+        } else {
+            Object key = list.removeLast();
+            map.remove(key);
         }
 
     }
